efbb63d342fdd1b5266399fb5e52b911ed77b0e1
favydabs <favourdabit@gmail.com>
2024-01-06 18:36:16 +0100

    webpack

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,28 +1,19 @@
-import $ from "jquery";
+import $ from 'jquery';
 import _ from 'lodash';
 
-
 $(document).ready(function(){
-  $('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button id="startButton">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-  $('body').append('<p>Dashboard data for the students</p>');
-  
-function updateCounter() {
-  let count = 0;
-  const paragraph = $('#count');
-
-  $('#clickButton').on('click', function() {
-    count++;
-    paragraph.text(`${count} clicks on the button`);
-  });
-
-  $('#clickLink').on('click', function(e) {
-    e.preventDefault();
-    updateCounter();
-  });
-}
-  $('body').append('<button id="startButton" >Click here to get started</button>', '<p id="count"{count}></p>');
-
-  $('body').append('<p>Copyright - Holberton School</p>');
+// Function to update counter and bind debounce
+let count = 0;
+const updateCounter = _.debounce(() => {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}, 300);
+// Bind debounce function to click event on the button
+$('#startButton').on('click', updateCounter);
 });
-
