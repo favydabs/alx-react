cb0d9fde7d453a3ac1ae4ec2207fde77d0e760f5
favydabs <favourdabit@gmail.com>
2024-01-06 19:00:40 +0100

    webpack

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button id="startButton">Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append('<p id="count">{count}</p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 // Function to update counter and bind debounce
--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: path.resolve(__dirname, 'js/dashboard_main.js'),
     output: {
         path:path.resolve(__dirname, 'public'),
