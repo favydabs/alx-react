bec4a32479f176357b639ab0900ce3be73e432cd
favydabs <favourdabit@gmail.com>
2024-01-06 10:56:44 +0100

    webpack

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,14 +1,27 @@
 import $ from "jquery";
+import _ from 'lodash';
 
 
 $(document).ready(function(){
   $('body').append('<p>Holberton Dashboard</p>');
 
   $('body').append('<p>Dashboard data for the students</p>');
+  
+function updateCounter() {
+  let count = 0;
+  const paragraph = $('#count');
 
-  $('body').append('<button id="startButton">Click here to get started</button>');
+  $('#clickButton').on('click', function() {
+    count++;
+    paragraph.text(`${count} clicks on the button`);
+  });
 
-  $('body').append('<p id="count"></p>');
+  $('#clickLink').on('click', function(e) {
+    e.preventDefault();
+    updateCounter();
+  });
+}
+  $('body').append('<button id="startButton" >Click here to get started</button>', '<p id="count"{count}></p>');
 
   $('body').append('<p>Copyright - Holberton School</p>');
 });
