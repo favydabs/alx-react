3046cd7c5a2786ee7977ec42bf6730030393ea7a
favydabs <favourdabit@gmail.com>
2024-01-06 08:13:29 +0100

    webpack

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,15 @@
+import $ from "jquery";
+
+
+$(document).ready(function(){
+  $('body').append('<p>Holberton Dashboard</p>');
+
+  $('body').append('<p>Dashboard data for the students</p>');
+
+  $('body').append('<button id="startButton">Click here to get started</button>');
+
+  $('body').append('<p id="count"></p>');
+
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
+
