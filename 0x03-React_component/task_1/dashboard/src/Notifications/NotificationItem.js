e3b233883761dc0c202da094fd0f05eb71940433
favydabs <favourdabit@gmail.com>
2024-01-22 16:19:32 +0100

    react component

---
+++ 0x03-React_component/task_1/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Notifications.css";
+
+function NotificationItem({ type, html, value }) {
+  return (
+    <>
+      {type && value ? <li data-notification-type={type}>{value}</li> : null}
+      {html ? (
+        <li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
+      ) : null}
+    </>
+  );
+}
+
+NotificationItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  value: PropTypes.string,
+};
+
+export default NotificationItem;
